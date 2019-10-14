961f4da6c2682f621633bc924f00d54f8eb7f2d1
Wildercat <abramgornik@gmail.com>
2019-10-14 09:45:47 -0400

    week 9 questions

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 var bodyDiv = document.getElementById('pagebody');
+function mkTag(tag, content) {
+    ht = document.createElement(tag);
+    ht.innerHTML = content;
+    return ht;
+}
 async function main() {
+    const NEWresponse = await fetch('new-article-content.json')
+    const NEWdata = await NEWresponse.json();
+    for (let i = NEWdata.content[0].length - 1; i >= 0; i--) {
+        let art = document.createElement('article');
+        art.setAttribute('class', 'row p-5');
+
+        // postheader column
+        let postHead = document.createElement('div');
+        postHead.setAttribute('class', 'col-lg-3 border-right');
+        // postheader title
+        let postTitle = document.createElement('h2');
+        postTitle.textContent = NEWdata.content[0][i].title;
+        //postheader date
+        let postDate = document.createElement('p');
+        postDate.textContent = NEWdata.content[0][i].date;
+        // append title and date to post header
+        postHead.appendChild(postTitle);
+        postHead.appendChild(postDate);
+
+
+        // post body column
+        let postBody = document.createElement('div');
+        postBody.setAttribute('class', 'col-lg');
+
+        NEWdata.content[0][i].body.map((section) => {
+            let div = mkTag('div','');
+            div.className = 'pb-3';
+            let headerText = section.header;
+            let textText = section.text;
+            let headerP = mkTag('p','');
+            // console.log(headerP);
+            headerP.appendChild(mkTag('b', headerText));
+            div.appendChild(headerP);
+            div.appendChild(mkTag('p', textText));
+            postBody.appendChild(div); 
+            
+        })
+
+        // append everything to article
+        art.appendChild(postHead);
+        art.appendChild(postBody)
+
+        // append article to body container
+        bodyDiv.appendChild(art);
+    }
     const response = await fetch('article-content.json')
     const data = await response.json();
     for (let i = data.content[0].length - 1; i >= 0; i--) {
@@ -32,7 +82,9 @@ async function main() {
         // append article to body container
         bodyDiv.appendChild(art);
     }
+
 }
+
 main();
 
 
